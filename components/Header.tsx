ec87e91f1f7ffb47e84e49d9057375bd08db4111
Andrey <tuguchev.andrey@gmail.com>
2023-12-14 02:00:28 +0600

    feat: dark mode on/off added

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,18 +3,20 @@
 import { SignedOut, UserButton, SignInButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
+import { ThemeToggler } from "./ThemeToggler"
 
 function Header() {
   return (
     <header className="flex items-center justify-between">
         <Link href="/" className="flex items-center space-x-2">
           <div className=" w-fit">  {/* bg-[#0160FE]*/}
-            <Image src="/next-dropbox-logo.png" alt="logo containing open box collored into dark blue" width={150} height={150} className="skiplazy w-[70px]" />
+            <Image src="/next-dropbox-logo.png" alt="logo containing open box collored into dark blue" width={150} height={150} className="skiplazy w-[70px] " />
           </div>
             <h1 className="font-bold text-xl">NextDropbox</h1>
         </Link>
         <div className="px-5 flex space-x-2 items-center">
           {/* Theme toggler */}
+          <ThemeToggler></ThemeToggler>
 
           <UserButton afterSignOutUrl="/"></UserButton>
           <SignedOut>
