588130fb3620919d6102e23fbec5b9ab655f63de
Andrey <tuguchev.andrey@gmail.com>
2023-12-14 01:46:55 +0600

    feat: shadcn/ui installed and clerk auth modal added

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { SignedOut, UserButton, SignInButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+
+function Header() {
+  return (
+    <header className="flex items-center justify-between">
+        <Link href="/" className="flex items-center space-x-2">
+          <div className=" w-fit">  {/* bg-[#0160FE]*/}
+            <Image src="/next-dropbox-logo.png" alt="logo containing open box collored into dark blue" width={150} height={150} className="skiplazy w-[70px]" />
+          </div>
+            <h1 className="font-bold text-xl">NextDropbox</h1>
+        </Link>
+        <div className="px-5 flex space-x-2 items-center">
+          {/* Theme toggler */}
+
+          <UserButton afterSignOutUrl="/"></UserButton>
+          <SignedOut>
+            <SignInButton afterSignInUrl="/dashboard" mode="modal" />
+          </SignedOut>
+        </div>
+    </header>
+  )
+}
+
+export default Header
